f3c5a99d326d3a88cde9a8c7592f0be98e946edd
sunkang23 <3412748970@qq.com>
2017-07-02 16:05:48 +0800

    Delete 1 lines
    Delete 1 lines

---
+++ app/src/main/java/com/example/sunkang/githubdemo/MainActivity.java
@@ -0,0 +1,19 @@
+package com.example.sunkang.githubdemo;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        System.out.println("hello,");
+        System.out.println("github!");
+
+        System.out.println("hello,github!");
+
+
+    }
+}
